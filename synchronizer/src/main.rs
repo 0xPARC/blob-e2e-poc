ebb4cb2c7e73d677a85d0f94f86bd8054570487a
Eduard S. <eduardsanou@posteo.net>
2025-09-09 21:30:10 +0200

    query txs and blobs

--- synchronizer/src/main.rs
+++ synchronizer/src/main.rs
@@ -1,6 +1,13 @@
 use std::{io, str::FromStr, time::Duration};
 
-use anyhow::{Result, anyhow};
+use alloy::{
+    consensus as alloy_consensus, eips as alloy_eips, network as alloy_network,
+    providers as alloy_provider,
+};
+use alloy_consensus::transaction::Transaction;
+use alloy_network::Ethereum;
+use alloy_provider::{Provider, ProviderBuilder, RootProvider};
+use anyhow::{Context, Result, anyhow};
 use backoff::ExponentialBackoffBuilder;
 use log::LevelFilter;
 use sqlx::{
@@ -9,7 +16,10 @@ use sqlx::{
     sqlite::{Sqlite, SqliteConnectOptions},
 };
 use synchronizer::clients::{
-    beacon::{self, BeaconClient, types::BlockId},
+    beacon::{
+        self, BeaconClient,
+        types::{BlockHeader, BlockId},
+    },
     common::{ClientError, ClientResult},
 };
 use tracing::{debug, info};
@@ -30,6 +40,7 @@ fn load_dotenv() -> Result<()> {
 #[derive(Debug)]
 pub struct Config {
     pub beacon_url: String,
+    pub rpc_url: String,
     pub sqlite_path: String,
     pub sync_start_slot: u32,
 }
@@ -44,6 +55,7 @@ impl Config {
         }
         Ok(Self {
             beacon_url: var("BEACON_URL")?,
+            rpc_url: var("RPC_URL")?,
             sqlite_path: var("SQLITE_PATH")?,
             sync_start_slot: u32::from_str(&var("SYNC_START_SLOT")?)?,
         })
@@ -66,7 +78,8 @@ async fn db_connection(url: &str) -> Result<SqlitePool, sqlx::Error> {
 #[derive(Debug)]
 struct Node {
     cfg: Config,
-    cli: BeaconClient,
+    beacon_cli: BeaconClient,
+    rpc_cli: RootProvider,
     db: SqlitePool,
 }
 
@@ -86,9 +99,81 @@ impl Node {
             base_url: cfg.beacon_url.clone(),
             exp_backoff,
         };
-        let cli = BeaconClient::try_with_client(http_cli, beacon_cli_cfg)?;
+        let beacon_cli = BeaconClient::try_with_client(http_cli, beacon_cli_cfg)?;
+        let rpc_cli = RootProvider::<Ethereum>::new_http(cfg.rpc_url.parse()?);
 
-        Ok(Self { cfg, db, cli })
+        Ok(Self {
+            cfg,
+            db,
+            beacon_cli,
+            rpc_cli,
+        })
+    }
+
+    async fn process_beeacon_block_header(
+        &self,
+        beacon_block_header: &BlockHeader,
+    ) -> Result<Option<()>> {
+        let beacon_block_root = beacon_block_header.root;
+        let slot = beacon_block_header.slot;
+
+        let beacon_block = match self.beacon_cli.get_block(BlockId::Slot(slot)).await? {
+            Some(block) => block,
+            None => {
+                info!("slot {} has empty block", slot);
+                return Ok(None);
+            }
+        };
+        let execution_payload = match beacon_block.execution_payload {
+            Some(payload) => payload,
+            None => {
+                info!("slot {} has no execution payload", slot);
+                return Ok(None);
+            }
+        };
+        info!(
+            "slot {} has execution block {} at height {}",
+            slot, execution_payload.block_hash, execution_payload.block_number
+        );
+
+        let has_kzg_blob_commitments = match beacon_block.blob_kzg_commitments {
+            Some(commitments) => !commitments.is_empty(),
+            None => false,
+        };
+        if has_kzg_blob_commitments {
+            info!("slot {} has no blobs", slot);
+            return Ok(None);
+        }
+
+        let execution_block_hash = execution_payload.block_hash;
+
+        let execution_block_id = alloy_eips::eip1898::BlockId::Hash(execution_block_hash.into());
+        let execution_block = self
+            .rpc_cli
+            .get_block(execution_block_id)
+            .full()
+            .await?
+            .with_context(|| format!("Execution block {execution_block_hash} not found"))?;
+
+        let blob_txs: Vec<_> = match execution_block.transactions.as_transactions() {
+            Some(txs) => txs
+                .into_iter()
+                .filter(|tx| tx.inner.blob_versioned_hashes().is_some())
+                .collect(),
+            None => {
+                return Err(anyhow!(
+                    "Consensus block {beacon_block_root} has blobs but the execution block doesn't have txs"
+                ));
+            }
+        };
+
+        if blob_txs.is_empty() {
+            return Err(anyhow!("Blocks mismatch: Consensus block \"{beacon_block_root}\" contains blob KZG commitments, but the corresponding execution block \"{execution_block_hash:#?}\" does not contain any blob transactions").into());
+        }
+
+        let blobs = self.beacon_cli.get_blobs(slot.into()).await?;
+        info!("found {} blobs", blobs.len());
+        Ok(Some(()))
     }
 }
 
@@ -104,28 +189,34 @@ async fn main() -> Result<()> {
     log_init();
     load_dotenv()?;
     let cfg = Config::from_env()?;
-    debug!(?cfg, "Loaded config");
+    info!(?cfg, "Loaded config");
 
     let node = Node::new(cfg).await?;
 
-    let spec = node.cli.get_spec().await?;
+    let spec = node.beacon_cli.get_spec().await?;
     info!(?spec, "Beacon spec");
     let head = node
-        .cli
+        .beacon_cli
         .get_block_header(BlockId::Head)
         .await?
         .expect("head is not None");
     info!(?head, "Beacon head");
 
     for slot in node.cfg.sync_start_slot..head.slot {
-        let block = match node.cli.get_block(BlockId::Slot(slot)).await? {
+        let beacon_block_header = match node
+            .beacon_cli
+            .get_block_header(BlockId::Slot(slot))
+            .await?
+        {
             Some(block) => block,
             None => {
-                debug!("slot {} has empty block", slot);
+                info!("slot {} has empty block", slot);
                 continue;
             }
         };
-        debug!(?block);
+
+        node.process_beeacon_block_header(&beacon_block_header)
+            .await?;
     }
 
     Ok(())
