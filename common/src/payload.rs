50f5811ae3276603df6a69612905d3a69ee23458
Eduard S. <eduardsanou@posteo.net>
2025-09-15 21:06:28 +0200

    wip

--- common/src/payload.rs
+++ common/src/payload.rs
@@ -10,7 +10,7 @@ use pod2::middleware::{
     C, CommonCircuitData, CustomPredicateBatch, CustomPredicateRef, D, F, Hash, RawValue,
 };
 
-fn write_elems<const N: usize>(bytes: &mut Vec<u8>, elems: &[F; N]) {
+pub fn write_elems<const N: usize>(bytes: &mut Vec<u8>, elems: &[F; N]) {
     for elem in elems {
         bytes
             .write_all(&elem.to_canonical_u64().to_le_bytes())
@@ -18,7 +18,7 @@ fn write_elems<const N: usize>(bytes: &mut Vec<u8>, elems: &[F; N]) {
     }
 }
 
-fn read_elems<const N: usize>(bytes: &mut impl Read) -> Result<[F; N]> {
+pub fn read_elems<const N: usize>(bytes: &mut impl Read) -> Result<[F; N]> {
     let mut elems = [F::ZERO; N];
     let mut elem_bytes = [0; 8];
     for i in 0..N {
@@ -30,6 +30,26 @@ fn read_elems<const N: usize>(bytes: &mut impl Read) -> Result<[F; N]> {
         elems[i] = F::from_canonical_u64(n);
     }
     Ok(elems)
+}
+
+pub fn write_custom_redicate_ref(bytes: &mut Vec<u8>, cpr: &CustomPredicateRef) {
+    write_elems(bytes, &cpr.batch.id().0);
+    bytes
+        .write_all(&(cpr.index as u8).to_le_bytes())
+        .expect("vec write");
+}
+
+pub fn read_custom_predicate_ref(bytes: &mut impl Read) -> Result<CustomPredicateRef> {
+    let custom_pred_batch_id = Hash(read_elems(bytes)?);
+    let custom_pred_index = {
+        let mut buffer = [0; 1];
+        bytes.read_exact(&mut buffer)?;
+        u8::from_le_bytes(buffer) as usize
+    };
+    Ok(CustomPredicateRef {
+        batch: CustomPredicateBatch::new_opaque("unknown".to_string(), custom_pred_batch_id),
+        index: custom_pred_index,
+    })
 }
 
 pub enum Payload {
@@ -87,20 +107,18 @@ impl Payload {
     }
 }
 
+#[derive(Debug)]
 pub struct PayloadInit {
-    id: Hash,
-    custom_predicate_ref: CustomPredicateRef,
-    vds_root: Hash,
-    state: RawValue,
+    pub id: Hash,
+    pub custom_predicate_ref: CustomPredicateRef,
+    pub vds_root: Hash,
+    pub state: RawValue,
 }
 
 impl PayloadInit {
     pub fn write_bytes(&self, buffer: &mut Vec<u8>) {
         write_elems(buffer, &self.id.0);
-        write_elems(buffer, &self.custom_predicate_ref.batch.id().0);
-        buffer
-            .write_all(&(self.custom_predicate_ref.index as u8).to_le_bytes())
-            .expect("vec write");
+        write_custom_redicate_ref(buffer, &self.custom_predicate_ref);
         write_elems(buffer, &self.vds_root.0);
         write_elems(buffer, &self.state.0);
     }
@@ -108,16 +126,7 @@ impl PayloadInit {
     pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
         let mut bytes = bytes;
         let id = Hash(read_elems(&mut bytes)?);
-        let custom_pred_batch_id = Hash(read_elems(&mut bytes)?);
-        let custom_pred_index = {
-            let mut buffer = [0; 1];
-            bytes.read_exact(&mut buffer)?;
-            u8::from_le_bytes(buffer) as usize
-        };
-        let custom_predicate_ref = CustomPredicateRef {
-            batch: CustomPredicateBatch::new_opaque("unknown".to_string(), custom_pred_batch_id),
-            index: custom_pred_index,
-        };
+        let custom_predicate_ref = read_custom_predicate_ref(&mut bytes)?;
         let vds_root = Hash(read_elems(&mut bytes)?);
         let state = RawValue(read_elems(&mut bytes)?);
         Ok(Self {
@@ -129,10 +138,11 @@ impl PayloadInit {
     }
 }
 
+#[derive(Debug)]
 pub struct PayloadUpdate {
-    id: Hash,
-    shrinked_main_pod_proof: CompressedProof<F, C, D>,
-    new_state: RawValue,
+    pub id: Hash,
+    pub shrinked_main_pod_proof: CompressedProof<F, C, D>,
+    pub new_state: RawValue,
 }
 
 impl PayloadUpdate {
@@ -153,7 +163,7 @@ impl PayloadUpdate {
             let mut buffer = Buffer::new(bytes);
             let proof =
                 plonky2::util::serialization::Read::read_compressed_proof(&mut buffer, common_data)
-                    .map_err(|e| anyhow!("read: {}", e))?;
+                    .map_err(|e| anyhow!("read_compressed_proof: {}", e))?;
             let len = buffer.pos();
             bytes = &bytes[len..];
             proof
