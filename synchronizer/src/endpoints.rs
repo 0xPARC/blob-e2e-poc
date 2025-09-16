6b8c28c49f4543eddd61ec345f1a117df068d2a8
Ahmad <root@ahmadafuni.com>
2025-09-16 10:30:21 +1000

    Code review

--- synchronizer/src/endpoints.rs
+++ synchronizer/src/endpoints.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use common::CustomError;
 use warp::Filter;
 
@@ -8,7 +10,7 @@ use crate::Node;
 // GET /ad_state/{id}
 pub(crate) async fn handler_get_ad_state(
     ad_id_str: String,
-    node: Node,
+    node: Arc<Node>,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     let ad_id = hex::decode(&ad_id_str).map_err(|e| CustomError(e.to_string()))?;
     let ad_state = node
@@ -22,12 +24,12 @@ pub(crate) async fn handler_get_ad_state(
 
 // build the routes
 pub(crate) fn routes(
-    node: Node,
+    node: Arc<Node>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     get_ad_state(node)
 }
 fn get_ad_state(
-    node: Node,
+    node: Arc<Node>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let node_filter = warp::any().map(move || node.clone());
 
